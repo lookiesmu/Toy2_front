06c55fa45314dade3fe855e0724a0d7d15ec202d
sooooz <soojinpar1026@gmail.com>
2020-08-19 02:38:36 +0900

    modify solve quiz dialog

--- Hackaton/app/src/main/java/com/example/hackaton/SolveQuizActivity.kt
+++ Hackaton/app/src/main/java/com/example/hackaton/SolveQuizActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hackaton
 
 import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_solve_quiz.*
@@ -32,24 +35,43 @@ class SolveQuizActivity : AppCompatActivity() {
         solve_quiz_recyclerview.layoutManager = LinearLayoutManager(this)
 
 
-
+        // 퀴즈 제출 버튼 클릭
         solve_quiz_finish.setOnClickListener {
             for (i in 0 until 5) {
                 Log.d("msgg",  ""+quizList[i].answer)
             }
 
+            // 이미 제출한 사용자일 경우
+            // if ()
+
+
+            var score: Int = 0
+
             val builder = AlertDialog.Builder(this)
             val dialogView = layoutInflater.inflate(R.layout.dialog_view, null)
+
+            // dialog score 변경
             val dialogScore = dialogView.findViewById<TextView>(R.id.dialog_score)
+            dialogScore.setText("$score 점")
 
-
+            // dialog 띄움
             builder.setView(dialogView)
-                .setPositiveButton("ADD") { dialog, i ->
+                .setPositiveButton("확인") { dialog, i ->
                     //val date = dialogDate.text.toString()
                     //val content = dialogContent.text.toString()
                     //planList.add(Plan(date, content))
+
+
+                    // score 전달
+                    val intent = Intent(this, MyifoActivity::class.java)
+                    intent.apply {
+                        this.putExtra("score", score)
+                    }
+                    startActivityForResult(intent, 200)
+
+                    // val num1 = intent.getIntExtra("num1", 0)
+
                 }
-                .setNegativeButton("BACK", null)
                 .show()
         }
 
